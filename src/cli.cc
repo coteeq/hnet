6993963c634cc05fae1b7f8d5e8b8130b7756faf
coteeq <isaqtm@gmail.com>
2023-05-01 13:53:11 +0000

    rewrite cli & srv to fibers

--- src/cli.cc
+++ src/cli.cc
@@ -1,17 +1,15 @@
-#include "five/xrange.h"
-#include "wheels/core/compiler.hpp"
-#include <numeric>
-#include <wheels/logging/logging.hpp>
-#include <string.h>
-#include <netring/net.h>
-#include <fallible/error/make.hpp>
-#include <fallible/error/throw.hpp>
-#include <wheels/core/assert.hpp>
+#include "ring2/submitter.h"
+#include "tf/rt/scheduler.hpp"
+#include <ring2/addr.h>
+#include <ring2/ring.h>
+#include <ring2/poller.h>
+#include <ring2/sock.h>
 #include <five/config.h>
-#include <wheels/core/assert.hpp>
 #include <five/program.h>
-#include <iostream>
 #include <five/time.h>
+#include <fallible/error/make.hpp>
+#include <wheels/logging/logging.hpp>
+#include <numeric>
 
 struct Address {
     std::string host;
@@ -24,11 +22,10 @@ struct Address {
 };
 TOML_STRUCT_DEFINE_SAVELOAD(Address);
 
-#define COMMA ,
 struct ClientConfig {
     std::string data_to_send = "ninebytes";
-    int64_t port = 6666;
-    Address server = Address{.host = "::1", .port = 6666};
+    int64_t port = 6667;
+    Address server = Address{.host = "::1", .port = 6667};
     int64_t count = 3;
 
     TOML_STRUCT_DEFINE_MEMBER_SAVELOAD(
@@ -43,59 +40,66 @@ TOML_STRUCT_DEFINE_SAVELOAD(ClientConfig);
 
 class ClientProgram: public Program<ClientConfig> {
     int Run(const ClientConfig& config) override {
-        auto socket = net::make_socket();
-        auto addr = net::Addr::from_parts(config.server.host, config.server.port);
+        auto ring = std::make_shared<net::Ring>();
+        auto poller = net::RingPoller(ring);
+        auto sched = tf::rt::Scheduler(&poller);
+        auto submitter = net::Submitter(ring);
+        sched.Run([&config, submitter] {
+            auto socket = net::UdpSocket();
+            auto addr = net::Addr::from_parts(config.server.host, config.server.port);
 
-        const char* data_to_send = config.data_to_send.c_str();
-        LOG_INFO("starting to send...");
+            // const char* data_to_send = config.data_to_send.c_str();
+            LOG_INFO("starting to send...");
+            LOG_INFO("target: {}, socketfd: {}", addr, socket.fd());
 
-        std::vector<five::Duration> timings;
-        timings.reserve(config.count);
+            std::vector<five::Duration> timings;
+            timings.reserve(config.count);
 
-        auto start = five::Instant::now();
-        for (int i = 0; i < config.count; ++i) {
-            WHEELS_UNUSED(i);
+            auto start = five::Instant::now();
+            for (int i = 0; i < config.count; ++i) {
+                WHEELS_UNUSED(i);
 
-            ssize_t len = sendto(
-                socket->fd(),
-                data_to_send,
-                config.data_to_send.size(),
-                0,
-                &addr.addr_,
-                addr.addrlen_
-            );
-            SYSCALL_VERIFY(len > 0, "sendto");
+                auto send_data = net::MsgHdr {
+                    .buf = config.data_to_send,
+                    .addr = addr,
+                };
+                LOG_INFO("sending...");
+                auto send_res = submitter.sendmsg(socket.fd(), send_data);
+                if (send_res <= 0) {
+                    fallible::ThrowError(fallible::Err(fallible::FromErrno{-send_res}, WHEELS_HERE));
+                }
+                LOG_INFO("sent! res={}", send_res);
 
-            // fprintf(stderr, "sent! len=%ld\n", len);
-            // fprintf(stderr, "receiving...\n");
+                auto result = submitter.recvmsg(socket.fd());
 
-            char buffer[150];
-            ssize_t len2 = recvfrom(socket->fd(), buffer, 99, 0, NULL, NULL);
+                if (result.buf == config.data_to_send) {
+                    return;
+                }
 
-            buffer[len2] = '\0';
-            if (std::string_view(buffer, len2) == config.data_to_send) {
-                return 0;
+                auto end = five::Instant::now();
+                timings.push_back(end - start);
+                std::swap(start, end);
+                // printf("received: '%s' (%zu bytes)\n", buffer, len2);
             }
 
-            auto end = five::Instant::now();
-            timings.push_back(end - start);
-            std::swap(start, end);
-            // printf("received: '%s' (%zu bytes)\n", buffer, len2);
-        }
+            if (config.count > 1000) {
+                timings = std::vector<five::Duration>(timings.begin() + 1000, timings.end());
+            }
 
-        std::sort(timings.begin(), timings.end());
+            std::sort(timings.begin(), timings.end());
 
-        LOG_INFO("min    = {}us", timings[0].micros());
-        LOG_INFO("max    = {}us", timings[timings.size() - 1].micros());
-        LOG_INFO(
-            "avg    = {}us",
-            std::accumulate(timings.begin(), timings.end(), five::Duration()).micros() / timings.size()
-        );
-        for (auto quantile : {50., 90., 95., 99., 99.9, 99.99}) {
-            auto idx = size_t(timings.size() * (quantile / 100.));
-            LOG_DEBUG("p{:<5} = {}us", quantile, timings[idx].micros());
-        }
-        wheels::FlushPendingLogMessages();
+            LOG_CRIT("min    = {}us", timings[0].micros());
+            LOG_CRIT("max    = {}us", timings[timings.size() - 1].micros());
+            LOG_CRIT(
+                "avg    = {}us",
+                std::accumulate(timings.begin(), timings.end(), five::Duration()).micros() / timings.size()
+            );
+            for (auto quantile : {50., 90., 95., 99., 99.9, 99.99}) {
+                auto idx = size_t(timings.size() * (quantile / 100.));
+                LOG_CRIT("p{:<5} = {}us", quantile, timings[idx].micros());
+            }
+        });
+
         return 0;
     }
 };
---
+++ src/srv2.cc
@@ -0,0 +1,47 @@
+#include <five/config.h>
+#include <five/program.h>
+#include <ring2/ring.h>
+#include <ring2/addr.h>
+#include <ring2/poller.h>
+#include <ring2/submitter.h>
+#include <ring2/sock.h>
+#include <tf/rt/scheduler.hpp>
+
+
+struct ServerConfig {
+    int64_t port = 6667;
+
+    TOML_STRUCT_DEFINE_MEMBER_SAVELOAD(
+        TS_VALUE(port)
+    )
+};
+
+TOML_STRUCT_DEFINE_SAVELOAD(ServerConfig);
+
+
+class ServerProgram: public Program<ServerConfig> {
+    int Run(const ServerConfig& config) override {
+        auto ring = std::make_shared<net::Ring>();
+        auto poller = net::RingPoller(ring);
+        auto sched = tf::rt::Scheduler(&poller);
+        auto submitter = net::Submitter(ring);
+
+        sched.Run([&config, submitter] {
+            auto socket = net::UdpSocket();
+            socket.bind(net::Addr::ip6_any(config.port));
+
+            for (;;) {
+                auto msg = submitter.recvmsg(socket.fd());
+                LOG_INFO("recv msg {} bytes", msg.buf.size());
+                msg.buf = msg.buf + msg.buf;
+                submitter.sendmsg(socket.fd(), msg);
+            }
+        });
+
+        return 0;
+    }
+};
+
+int main(int argc, char **argv) {
+    ServerProgram().SetupAndRun(argc, argv);
+}
