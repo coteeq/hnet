50ded86b321d99a7d4ec3a690bcdcec704886baf
coteeq <isaqtm@gmail.com>
2023-05-08 10:31:44 +0000

    multi mode srv

--- src/srv2.cc
+++ src/srv2.cc
@@ -1,4 +1,6 @@
+#include "wheels/core/assert.hpp"
 #include "wheels/logging/logging.hpp"
+#include "wheels/memory/view_of.hpp"
 #include <five/config.h>
 #include <five/program.h>
 #include <ring2/ring.h>
@@ -12,9 +14,15 @@
 
 struct ServerConfig {
     int64_t port = 6667;
+    int64_t ring_entries = 8;
+    std::string mode = "udp";
+    int64_t ipv = 4;
 
     TOML_STRUCT_DEFINE_MEMBER_SAVELOAD(
         TS_VALUE(port)
+        TS_VALUE(ring_entries)
+        TS_VALUE(mode)
+        TS_VALUE(ipv)
     )
 };
 
@@ -26,35 +34,64 @@ const char REPLY[] = \
     // "Connection: close\r\n"
     "\r\nninebytes";
 
-class ServerProgram: public Program<ServerConfig> {
+class ServerProgram: public five::Program<ServerConfig> {
     int Run(const ServerConfig& config) override {
-        auto ring = std::make_shared<net::Ring>();
+        auto ring = std::make_shared<net::Ring>(config.ring_entries);
         auto poller = net::RingPoller(ring);
         auto sched = tf::rt::Scheduler(&poller);
         auto submitter = net::Submitter(ring);
 
         sched.Run([&config, submitter] {
-            auto socket = net::Socket(net::IPFamily::V4, net::Proto::TCP);
-            socket.bind(net::Addr::ip4_any(config.port));
-            socket.listen(10);
+            auto sock_family = config.ipv == 4 ? net::IPFamily::V4 : net::IPFamily::V6;
+            auto bind_addr = config.ipv == 4 ? net::Addr::ip4_any(config.port) : net::Addr::ip6_any(config.port);
 
-            for (;;) {
-                auto [fd, addr] = submitter.accept(socket.fd());
-                LOG_INFO("Got connection from {}", addr);
-                tf::Spawn([&submitter, fd=fd] {
-                    for (;;) {
-                        auto req = submitter.recvmsg(fd);
-                        if (req.buf.size() == 0) {
-                            break;
+            if (config.mode == "tcp") {
+                auto socket = net::Socket(sock_family, net::Proto::TCP);
+                socket.bind(bind_addr);
+                socket.listen(10);
+
+                for (;;) {
+                    auto [fd, addr] = submitter.accept(socket.fd());
+                    LOG_INFO("Got connection from {}", addr);
+                    tf::Spawn([&submitter, fd=fd, addr=addr] {
+
+                        for (;;) {
+                            auto req = submitter.recvmsg(fd);
+                            if (req.raw_ret == -ECONNRESET) {
+                                LOG_ERROR("Conn reset for addr {}", addr);
+                                break;
+                            }
+                            if (req.buf.size() == 0) {
+                                break;
+                            }
+                            LOG_INFO("Received req: from: {}, text: {} raw_ret = {}", req.addr, req.buf, req.raw_ret);
+                            net::MsgHdr hdr = {
+                                .buf = REPLY,
+                                .addr = req.addr,
+                            };
+                            submitter.sendmsg(fd, hdr);
                         }
-                        LOG_INFO("Received req: from: {}, text: {} raw_ret = {}", req.addr, req.buf, req.raw_ret);
-                        net::MsgHdr hdr = {
-                            .buf = REPLY,
-                            .addr = req.addr,
-                        };
-                        submitter.sendmsg(fd, hdr);
+                    }).Detach();
+                }
+            } else if (config.mode == "udp") {
+                auto socket = net::Socket(sock_family, net::Proto::UDP);
+                socket.bind(bind_addr);
+
+                for (;;) {
+                    char buf[65'000];
+                    auto msg = submitter.recvmsg(socket.fd(), wheels::MutViewOf(buf));
+                    LOG_INFO("recv msg {} bytes", msg.buf.Size());
+                    WHEELS_VERIFY(msg.buf.Size() * 2 < sizeof(buf), "Too long message");
+                    {
+                        auto* second = buf + msg.buf.Size();
+                        memcpy(second, buf, msg.buf.Size());
                     }
-                }).Detach();
+                    auto ww = net::WriteView {
+                        .buf = wheels::ConstMemView{buf, msg.buf.Size() * 2},
+                        .addr = msg.addr
+                    };
+                    submitter.sendmsg(socket.fd(), ww);
+                }
             }
         });
 
